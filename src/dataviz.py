508be0e1941bb614bc9137e55bf28e095104733e
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-01-02 09:00:12 -0300

    :sparkles: feat: novos módulos de teste ok

--- src/dataviz.py
+++ src/dataviz.py
@@ -4,8 +4,22 @@ import matplotlib.pyplot as plt
 
 # Carregar os dados
 profissoes = pd.read_csv('docs_silver/profissoes.csv')
+animais = pd.read_csv('docs_silver/animais.csv')
+armas = pd.read_csv('docs_silver/armas.csv')
+artesanatos = pd.read_csv('docs_silver/artesanatos.csv')
 
 # Título da aplicação
 st.title('Profissões')
-
 st.dataframe(profissoes)
+
+st.title('Animais')
+st.dataframe(animais)
+
+st.title('Armas')
+st.dataframe(armas)
+
+st.title('Artesanato')
+st.dataframe(artesanatos)
+
+
+#streamlit run src/dataviz.py
