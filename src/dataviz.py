47a7875988e63cc13f4ab23082a88f4fbf148887
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-01-04 12:12:57 -0300

    :bulb: docs: adicionando comentários de ideias futuras.

--- src/dataviz.py
+++ src/dataviz.py
@@ -23,3 +23,25 @@ st.dataframe(artesanatos)
 
 
 #streamlit run src/dataviz.py
+
+
+
+"""def grafico_xp ():
+  #xp cultivo
+  sns.boxplot(data=df_xp_cultivos, y='Estacao', x='XP', hue='Estacao')
+
+  df_xp_cultivos = df_xp_cultivos.sort_values(by='XP', ascending=False)
+  with sns.axes_style('whitegrid'):
+    grafico = sns.barplot(data=df_xp_cultivos, y='Cultivo', x='XP', hue='Estacao', width = 0.8)
+    grafico.set_title('XP por cultivo')
+    grafico.set_xlabel('XP')
+    grafico.set_ylabel('Cultivo')
+    #tamanho do gráfico
+    grafico.figure.set_size_inches(10, 20)
+    
+  #qualidade cultivo
+  #fixar cores da legenda
+  
+  pass
+
+"""
