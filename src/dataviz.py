21b8b46c3a5af9cd9da08705ae3c917ba08b9b72
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-01-17 08:27:16 -0300

    :sparkles: feat: separando as páginas em multipage. Falta criar um menu principal de navegação

--- src/dataviz.py
+++ src/dataviz.py
@@ -26,11 +26,28 @@ def main():
   iscas = pd.read_csv('docs_silver/iscas.csv',encoding='utf-8')
 
 
-  # Título da aplicação
   st.markdown('Profissões')
   st.sidebar.markdown('Profissões')
   st.title('Profissões')
   st.dataframe(profissoes)
+
+  st.title('XP')
+  def filter_dataframe(df, selected_values):
+      if selected_values:
+          filtered_df = df[df['Profissao'].isin(selected_values)]
+      else:
+          filtered_df = df
+      return filtered_df
+
+  selected_values = st.multiselect('Selecione filtro por Profissão:', xp['Profissao'].unique())# Create a multiselect widget for selecting values
+  filtered_xp = filter_dataframe(xp, selected_values)# Filter the DataFrame based on selected values
+
+  st.dataframe(filtered_xp)# Display the filtered DataFrame
+  st.bar_chart(filtered_xp,x='item',
+              y='XP',
+              x_label='Item',
+              y_label='Valor de Experiência',
+              horizontal=True)
 
   st.markdown('Animais')
   st.sidebar.markdown('Animais')
@@ -66,6 +83,11 @@ def main():
   st.sidebar.markdown('Calendário')
   st.title('Calendário')
   st.dataframe(calendario)
+
+  st.markdown('Caverna')
+  st.sidebar.markdown('Caverna')
+  st.title('Caverna')
+  st.dataframe(caverna)
 
   st.markdown('Carteira')
   st.sidebar.markdown('Carteira')
@@ -114,23 +136,7 @@ def main():
   
   # fim da função
 
-  st.title('XP')
-  def filter_dataframe(df, selected_values):
-      if selected_values:
-          filtered_df = df[df['Profissao'].isin(selected_values)]
-      else:
-          filtered_df = df
-      return filtered_df
-
-  selected_values = st.multiselect('Selecione filtro por Profissão:', xp['Profissao'].unique())# Create a multiselect widget for selecting values
-  filtered_xp = filter_dataframe(xp, selected_values)# Filter the DataFrame based on selected values
-
-  st.dataframe(filtered_xp)# Display the filtered DataFrame
-  st.bar_chart(filtered_xp,x='item',
-              y='XP',
-              x_label='Item',
-              y_label='Valor de Experiência',
-              horizontal=True)
+  
 
   st.title('Árvores')
   st.dataframe(arvores)
