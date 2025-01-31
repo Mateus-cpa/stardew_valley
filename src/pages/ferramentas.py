28541c31a3b6d7fa773f0294343ad31dfce54816
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-01-31 12:24:25 -0300

    :sparkles: feat: dataframe pronto

--- src/pages/ferramentas.py
+++ src/pages/ferramentas.py
@@ -26,120 +26,16 @@ def main():
   iscas = pd.read_csv('docs_silver/iscas.csv',encoding='utf-8')
 
 
-  st.markdown('Profissões')
-  st.sidebar.markdown('Profissões')
-  st.title('Profissões')
-  st.dataframe(profissoes)
-
-  st.title('XP')
-  def filter_dataframe(df, selected_values):
-      if selected_values:
-          filtered_df = df[df['Profissao'].isin(selected_values)]
-      else:
-          filtered_df = df
-      return filtered_df
-
-  selected_values = st.multiselect('Selecione filtro por Profissão:', xp['Profissao'].unique())# Create a multiselect widget for selecting values
-  filtered_xp = filter_dataframe(xp, selected_values)# Filter the DataFrame based on selected values
-
-  st.dataframe(filtered_xp)# Display the filtered DataFrame
-  st.bar_chart(filtered_xp,x='item',
-              y='XP',
-              x_label='Item',
-              y_label='Valor de Experiência',
-              horizontal=True)
-
-  st.markdown('Animais')
-  st.sidebar.markdown('Animais')
-  st.title('Animais')
-  st.dataframe(animais)
-
-  st.markdown('Armas')
-  st.sidebar.markdown('Armas')
-  st.title('Armas')
-  st.dataframe(armas)
-
-  st.markdown('Atributos de Luta')
-  st.sidebar.markdown('Atributos de Luta')
-  st.title('Atributos de Luta')
-  st.dataframe(atributos_luta)
-
-  st.markdown('Produtos')
-  st.sidebar.markdown('Produtos')
-  st.title('Produtos')
-  st.dataframe(produtos)
-
-  st.markdown('Artefatos')
-  st.sidebar.markdown('Artefatos')
-  st.title('Artefatos')
-  st.dataframe(artefatos)
-
-  st.markdown('Vestuário')
-  st.sidebar.markdown('Vestuário')
-  st.title('Vestuário')
-  st.dataframe(vestuarios)
-
-  st.markdown('Calendário')
-  st.sidebar.markdown('Calendário')
-  st.title('Calendário')
-  st.dataframe(calendario)
-
-  st.markdown('Caverna')
-  st.sidebar.markdown('Caverna')
-  st.title('Caverna')
-  st.dataframe(caverna)
-
-  st.markdown('Carteira')
-  st.sidebar.markdown('Carteira')
-  st.title('Carteira')
-  st.dataframe(carteira)
-
-  st.markdown('Clima')
-  st.sidebar.markdown('Clima')
-  st.title('Clima')
-  st.dataframe(clima)
-
-  st.markdown('Coleta')
-  st.sidebar.markdown('Coleta')
-  st.title('Coleta')
-  st.dataframe(coleta)
-
-  st.markdown('Conjunto')
-  st.sidebar.markdown('Conjunto')
-  st.title('Conjunto')
-  st.dataframe(conjunto)
-
-  st.markdown('Culinária')
-  st.sidebar.markdown('Culinária')
-  st.title('Culinária')
-  st.dataframe(culinaria)
-
-  st.markdown('Solo foliar')
-  st.sidebar.markdown('Solo foliar')
-  st.title('Solo foliar')
-  st.dataframe(solo_foliar)
-
-  st.markdown('Casa')
-  st.sidebar.markdown('Casa')
-  st.title('Casa')
-  st.dataframe(casa)
-
+  # Título
   st.markdown('Ferramentas')
   st.sidebar.markdown('Ferramentas')
   st.title('Ferramentas')
   st.dataframe(ferramentas)
 
-  st.markdown('Iscas')
-  st.sidebar.markdown('Iscas')
-  st.title('Iscas')
-  st.dataframe(iscas)
   
   # fim da função
 
   
-
-  st.title('Árvores')
-  st.dataframe(arvores)
 
 if __name__ == '__main__':
     main()
