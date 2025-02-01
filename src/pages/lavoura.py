33071cabfa0a2b8f371e022c57d35ed933272f14
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-02-01 09:03:35 -0300

    :bug: fix: preparando página com dataframe

--- src/pages/lavoura.py
+++ src/pages/lavoura.py
@@ -41,39 +41,11 @@ def main():
 if __name__ == '__main__':
     main()
 """
-streamlit run src/dataviz.py
-
-
-
-your-repository/
-├── page_1.py
-├── page_2.py
-└── streamlit_app.py
+streamlit run src/pages/lavoura.py
 
 pg = st.navigation([st.Page("page_1.py"), st.Page("page_2.py")])
 pg.run()
 https://docs.streamlit.io/develop/concepts/multipage-apps/page-and-navigation
 https://docs.streamlit.io/develop/quick-reference/cheat-sheet
-
-"""
-
-
-"""def grafico_xp ():
-  #xp cultivo
-  sns.boxplot(data=df_xp_cultivos, y='Estacao', x='XP', hue='Estacao')
-
-  df_xp_cultivos = df_xp_cultivos.sort_values(by='XP', ascending=False)
-  with sns.axes_style('whitegrid'):
-    grafico = sns.barplot(data=df_xp_cultivos, y='Cultivo', x='XP', hue='Estacao', width = 0.8)
-    grafico.set_title('XP por cultivo')
-    grafico.set_xlabel('XP')
-    grafico.set_ylabel('Cultivo')
-    #tamanho do gráfico
-    grafico.figure.set_size_inches(10, 20)
-    
-  #qualidade cultivo
-  #fixar cores da legenda
-  
-  pass
 
 """
