e745dce77a7bac09218115a6f25c7594f42ccdd6
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-02-15 08:30:55 -0300

    :bromm: feat: retira taxa_cultivo e o inclui em producao_solos. Altera pescas

--- src/pages/pescas.py
+++ src/pages/pescas.py
@@ -6,15 +6,21 @@ def main():
   if st.button('Home'):
     st.switch_page("dataviz.py")
   # Carregar os dados
-  pescas = pd.read_csv('docs_silver/pescas.csv',encoding='utf-8')
-
+  pescas_bau = pd.read_csv('docs_silver/pescas_bau.csv',encoding='utf-8')
+  pescas_zona = pd.read_csv('docs_silver/pescas_zona.csv',encoding='utf-8')
 
   # Título
-  st.title('Pescas')
-  st.metric('Tamanho da tabela', pescas.shape[0])
-  st.metric('Quantidade de colunas', pescas.shape[1])
+  st.title('Pescas de Baús')
+  st.metric('Tamanho da tabela', pescas_bau.shape[0])
+  st.metric('Quantidade de colunas', pescas_bau.shape[1])
 
-  st.dataframe(pescas, use_container_width=True,hide_index=True)
+  st.dataframe(pescas_bau, use_container_width=True,hide_index=True)
+
+  st.title('Zonas de Pesca')
+  st.metric('Tamanho da tabela', pescas_zona.shape[0])
+  st.metric('Quantidade de colunas', pescas_zona.shape[1])
+
+  st.dataframe(pescas_zona, use_container_width=True,hide_index=True)
 
   # fim da função
 
--- src/pages/producao_solos.py
+++ src/pages/producao_solos.py
@@ -7,6 +7,7 @@ def main():
     st.switch_page("dataviz.py")
   # Carregar os dados
   producao_solos = pd.read_csv('docs_silver/solos_producao.csv',encoding='utf-8')
+  taxa_cultivo = pd.read_csv('docs_silver/taxas_cultivo.csv',encoding='utf-8')
 
 
   # Título
@@ -15,6 +16,16 @@ def main():
   st.metric('Quantidade de colunas', producao_solos.shape[1])
 
   st.dataframe(producao_solos, use_container_width=True,hide_index=True)
+
+
+
+  # Título
+  st.title('Taxas de cultivo')
+  st.metric('Tamanho da tabela', taxa_cultivo.shape[0])
+  st.metric('Quantidade de colunas', taxa_cultivo.shape[1])
+
+  st.dataframe(taxa_cultivo, use_container_width=True,hide_index=True)
+
 
   # fim da função
 
