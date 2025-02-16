777feb05d9cb35b9220e8e20af974999a7a3e338
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-02-16 07:31:37 -0300

    :sparkles: feat: retirando colunas do df e incluindo gráfico

--- src/pages/animais.py
+++ src/pages/animais.py
@@ -7,7 +7,7 @@ def main():
     st.switch_page("dataviz.py")
   # Carregar os dados
   animais = pd.read_csv('docs_silver/animais.csv',encoding='utf-8')
-
+  animais = animais.drop(columns=['Unnamed: 0'])
 
   # Título
   st.title('Animais')
@@ -17,11 +17,23 @@ def main():
   st.metric('Custo médio de animais', custo_animais)
   valor_produtos = round(animais.Produz_valor.mean())
   st.metric('Valor médio dos produtos', valor_produtos)
-  st.metric('Rendimento médio de animal / produto', round(custo_animais/valor_produtos,2))
+  st.metric('Rendimento médio de produção', round(animais['Rendimento de produção'].mean(),2))
+  st.metric('Rendimento por venda', round(animais['Rendimento de venda'].mean(),2))
   
-  st.dataframe(animais)
+  st.dataframe(animais, 
+               hide_index=True, 
+               on_select='rerun',
+               selection_mode='multi-row')
+  
+  st.subheader('Gráfico de rendimento de produtos gerados')
+  fig, ax = plt.subplots()
+  #plotar rendimento de produção por nome
+  animais = animais.sort_values(by='Rendimento de produção', ascending=False)
+  animais.plot(kind='bar', ax=ax, x='Nome', y='Rendimento de produção')
+  st.pyplot(fig)
   
   
+
   # fim da função
 
   
