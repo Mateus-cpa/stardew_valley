9e87850b7ec1a472c550e48f1b3799f631c6b895
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-02-22 07:28:50 -0300

    :sparkles: feat: criando filtros e gráficos em colunas

--- src/pages/armas.py
+++ src/pages/armas.py
@@ -1,23 +1,55 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def main():
-  if st.button('Home'):
-    st.switch_page("dataviz.py")
-  # Carregar os dados
-  armas = pd.read_csv('docs_silver/armas.csv',encoding='utf-8')
+    if st.button('Home'):
+        st.switch_page("dataviz.py")
+    # Carregar os dados
+    armas = pd.read_csv('docs_silver/armas.csv', encoding='utf-8')
+    # Retirar primeira coluna
+    armas = armas.iloc[:, 1:]
+    
+    # Título
+    st.title('Armas')
+    
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.metric('Tamanho da tabela', armas.shape[0])
+    with col2:
+        st.metric('Quantidade de colunas', armas.shape[1])
+    with col3:
+        st.metric('Dano médio', round(armas['Dano médio'].mean(), 2))
+    with col4:
+        st.metric('Valor de arma por dano médio', round(armas['Valor por dano'].mean(), 2))
+    
+    # Exibir dataframe com a quantidade de linhas selecionada
+    st.dataframe(armas, hide_index=True)
+    
+    # Slider para selecionar a quantidade de linhas a serem exibidas
+    num_linhas = st.slider('Quantidade de linhas a serem exibidas', min_value=1, max_value=armas.shape[0], value=armas.shape[0])
+    ascending = st.checkbox('Crescente', value=False)
 
+    col5, col6 = st.columns(2)
 
-  # Título
-  st.title('Armas')
-  st.metric('Tamanho da tabela', armas.shape[0])
-  st.metric('Quantidade de colunas', armas.shape[1])
+    with col5:
+        st.metric('Dano médio', round(armas['Dano médio'].head(num_linhas).mean(), 2))
+        
+        # Gráfico de dano
+        fig, ax = plt.subplots()
+        armas.sort_values(by='Dano médio', ascending=ascending).head(num_linhas).plot(kind='bar', ax=ax, x='Nome', y='Dano médio', grid=True)
+        st.pyplot(fig)
 
-  st.dataframe(armas)
+    with col6:
+        st.metric('Valor de arma por dano médio', round(armas['Valor por dano'].dropna().head(num_linhas).mean(), 2))
+    
+        # Gráfico de valor por dano
+        fig, ax = plt.subplots()
+        armas.sort_values(by='Valor por dano', ascending=ascending).dropna(subset='Valor por dano').head(num_linhas).plot(kind='bar', ax=ax, x='Nome', y='Valor por dano', grid=True)
+        st.pyplot(fig)
 
-  # fim da função
-
-  
+    # Exibir dataframe com a quantidade de linhas selecionada
+    st.dataframe(armas.head(num_linhas), hide_index=True)
 
 if __name__ == '__main__':
     main()
