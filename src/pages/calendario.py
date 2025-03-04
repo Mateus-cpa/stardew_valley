ce5abaa310deb59e3ff07d823b479d5c55a0c77d
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-03-04 08:58:36 -0300

    :broom: feat: calendario pronto

--- src/pages/calendario.py
+++ src/pages/calendario.py
@@ -6,16 +6,26 @@ def main():
   if st.button('Home'):
     st.switch_page("dataviz.py")
   # Carregar os dados
-  calendario = pd.read_csv('docs_silver/calendario.csv',encoding='utf-8')
+  calendario = pd.read_csv('docs_silver/calendario.csv',encoding='utf-8').iloc[:,1:]
 
 
   # Título
   st.title('Calendário')
-  col1, col2 = st.columns(2)
-  col1.metric('Tamanho da tabela', calendario.shape[0])
-  col2.metric('Quantidade de colunas', calendario.shape[1])
 
-  st.dataframe(calendario)
+  #filtro por evento
+  filtro_evento = st.multiselect('Filtro por evento', calendario['Evento'].unique())
+  filtro_mes = st.multiselect('Filtro por Mês', calendario['Mes'].unique())
+  if filtro_evento:
+    calendario = calendario[calendario['Evento'].isin(filtro_evento)]
+  if filtro_mes:
+    calendario = calendario[calendario['Mes'].isin(filtro_mes)]
+
+  col1, col2, col3 = st.columns(3)
+  col1.metric('Total de Eventos', calendario.shape[0])
+  col2.metric('Aniversários', calendario[calendario['Evento'] == 'aniversario'].shape[0])
+  col3.metric('Festivais', calendario[calendario['Evento'] == 'festivais'].shape[0])
+
+  st.dataframe(calendario, hide_index=True)
 
   # fim da função
 
