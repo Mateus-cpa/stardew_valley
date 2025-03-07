1d13af1bc81b5e5a81dbb48a7b9303c0bb01cf1c
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-03-07 17:05:08 -0300

    :sparkles: feat: página funcionando.

--- src/pages/clima.py
+++ src/pages/clima.py
@@ -6,15 +6,22 @@ def main():
   if st.button('Home'):
     st.switch_page("dataviz.py")
   # Carregar os dados
-  clima = pd.read_csv('docs_silver/clima.csv',encoding='utf-8')
+  clima = pd.read_csv('docs_silver/clima.csv',encoding='utf-8').iloc[:,1:]
 
 
   # Título
   st.title('Clima')
-  st.metric('Tamanho da tabela', clima.shape[0])
-  st.metric('Quantidade de colunas', clima.shape[1])
+  
+  filtro_texto = st.text_input('Filtrar resultados por um termo')
+  if filtro_texto:
+    clima = clima[clima.astype(str).apply(lambda x: x.str.contains(filtro_texto, case=False, na=False)).any(axis=1)].reset_index(drop=True)
+  st.metric('Quantidade de resultados', clima.shape[0])
 
-  st.dataframe(clima)
+  if clima.shape[0] == 1:
+    st.subheader(f'{clima["Clima"].values[0]}')
+    st.text(f'Descrição: {clima["Descrição"].values[0]}')
+  else:
+    st.dataframe(clima, hide_index=True)
 
   # fim da função
 
