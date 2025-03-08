93e4ef8ee1b5f31e6a3f9de4239f820af33de3ce
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-03-08 08:52:12 -0300

    :wrench: feat: alguns filtros e dataframe funcionando. Falta arrumar kpis e gráficos

--- src/pages/culinaria.py
+++ src/pages/culinaria.py
@@ -6,15 +6,29 @@ def main():
   if st.button('Home'):
     st.switch_page("dataviz.py")
   # Carregar os dados
-  culinaria = pd.read_csv('docs_silver/culinaria.csv',encoding='utf-8')
+  culinaria = pd.read_csv('docs_silver/culinaria.csv',encoding='utf-8').iloc[:,1:]
 
 
   # Título
   st.title('Culinária')
-  st.metric('Tamanho da tabela', culinaria.shape[0])
-  st.metric('Quantidade de colunas', culinaria.shape[1])
 
-  st.dataframe(culinaria)
+  #Filtro
+  filtro = st.multiselect('Filtre por tipos de ingredientes', culinaria.Tipo.unique())
+  if filtro:
+    culinaria = culinaria[culinaria.Tipo.isin(filtro)]
+  # Retirar colunas que todos dados estão em branco
+  culinaria = culinaria.dropna(axis=1, how='all')
+  st.dataframe(culinaria, hide_index=True)
+
+  # KPIs
+  col1, col2, col3 = st.columns(3)
+  col1.metric('Quantidade de resultados', culinaria.shape[0])
+  col2.metric('Quantidade de colunas', culinaria.shape[1])
+  if 'Preço de Venda' in culinaria.columns:
+    st.metric('Preço médio de venda', culinaria['Preço de Venda'].mean())
+
+
+  st.dataframe(culinaria, hide_index=True)
 
   # fim da função
 
