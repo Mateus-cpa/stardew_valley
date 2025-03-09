fe9ba3e4d2c9eb99d21d4a1a72d92ad1631e6d0e
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-03-09 17:23:04 -0300

    :sparkles: feat: filtro de valores e gráfico adicionado

--- src/pages/culinaria.py
+++ src/pages/culinaria.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import altair as alt
 
 def main():
   if st.button('Home'):
@@ -16,19 +17,36 @@ def main():
   filtro = st.multiselect('Filtre por tipos de ingredientes', culinaria.Tipo.unique())
   if filtro:
     culinaria = culinaria[culinaria.Tipo.isin(filtro)]
+  #fazer um filtro slider de preço de venda
+  filtrar_por_venda = st.checkbox('Filtrar por preço de venda')
+  if filtrar_por_venda:
+    filtro_preco = st.slider('Filtre por preço de venda:', culinaria['Preço de Venda'].min(), culinaria['Preço de Venda'].max(), (culinaria['Preço de Venda'].min(), culinaria['Preço de Venda'].max()))  
+    culinaria = culinaria[(culinaria['Preço de Venda'] >= filtro_preco[0]) & (culinaria['Preço de Venda'] <= filtro_preco[1])]
   # Retirar colunas que todos dados estão em branco
   culinaria = culinaria.dropna(axis=1, how='all')
-  st.dataframe(culinaria, hide_index=True)
 
   # KPIs
-  col1, col2, col3 = st.columns(3)
+  col1, col2, col3, col4 = st.columns(4)
   col1.metric('Quantidade de resultados', culinaria.shape[0])
-  col2.metric('Quantidade de colunas', culinaria.shape[1])
   if 'Preço de Venda' in culinaria.columns:
-    st.metric('Preço médio de venda', culinaria['Preço de Venda'].mean())
+    col2.metric('Preço médio de venda', round(culinaria['Preço de Venda'].mean()))
+  col3.metric('Saúde (média)', round(culinaria['Saúde'].mean()))
+  col4.metric('Energia (média)', round(culinaria['Energia'].mean()))
 
-
+  #dataframe
   st.dataframe(culinaria, hide_index=True)
+
+  #gráfico
+  
+  st.write('Preço de venda')
+  st.write(alt.Chart(culinaria).mark_point().encode(
+    x='Saúde',
+    y='Energia',
+    color='Preço de Venda'
+  ).properties(
+    width=800,
+    height=400
+  ))
 
   # fim da função
 
