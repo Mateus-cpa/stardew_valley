d738d837374bf507c4039fcfb785a4b369e2c1e0
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-03-13 18:21:38 -0300

    :sparkles: feat: página funcionando

--- src/pages/ferramentas.py
+++ src/pages/ferramentas.py
@@ -6,23 +6,31 @@ def main():
   if st.button('Home'):
     st.switch_page("dataviz.py")
   # Carregar os dados
-  ferramentas = pd.read_csv('docs_silver/ferramentas.csv',encoding='utf-8')
+  ferramentas = pd.read_csv('docs_silver/ferramentas.csv',encoding='utf-8').iloc[:,1:]
 
 
   # Título
   st.title('Ferramentas')
   
-  # Filtro
-  filtro_texto = st.text_input('Filtro por um termo')
+  
+  # Cria colunas
+  col1, col2, col3 = st.columns(3)
+  
+# Filtro
+  filtro_texto = col1.text_input('Filtro por um termo')
   if filtro_texto:
      ferramentas = ferramentas[ferramentas.astype(str).apply(lambda x: x.str.contains(filtro_texto, case=False, na=False)).any(axis=1)].reset_index(drop=True)
 
-  col1, col2, col3 = st.columns(3)
 
+  # select
+  filtro_select = col3.multiselect('Filtro por Tipo', ferramentas['Tipo'].unique())
+  if filtro_select:
+    ferramentas = ferramentas[ferramentas['Tipo'].isin(filtro_select)].reset_index(drop=True)
+    
+  #KPIs
   col2.metric('Quantidade de resultados', ferramentas.shape[0])
 
-  
-  st.dataframe(ferramentas)
+  st.dataframe(ferramentas, hide_index=True)
 
   # fim da função
 
