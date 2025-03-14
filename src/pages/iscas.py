2b1aebad40e7a647d2cbfaee0e3f16267b2009eb
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-03-14 07:06:22 -0300

    :wrench: feat: início das alterações, nada relevante ainda

--- src/pages/iscas.py
+++ src/pages/iscas.py
@@ -6,7 +6,7 @@ def main():
   if st.button('Home'):
     st.switch_page("dataviz.py")
   # Carregar os dados
-  iscas = pd.read_csv('docs_silver/iscas.csv',encoding='utf-8')
+  iscas = pd.read_csv('docs_silver/iscas.csv',encoding='utf-8').iloc[:,1:]
 
 
   # Título
@@ -14,7 +14,7 @@ def main():
   st.metric('Tamanho da tabela', iscas.shape[0])
   st.metric('Quantidade de colunas', iscas.shape[1])
 
-  st.dataframe(iscas)
+  st.dataframe(iscas, hide_index=True)
 
   # fim da função
 
