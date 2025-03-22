42ad79d1f6817be09cac04debbdfbddd6acede53
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-03-22 08:25:40 -0300

    :sparkles: feat: visual melhorado, necessita separação dos itens nas colunas

--- src/pages/lista_presentes.py
+++ src/pages/lista_presentes.py
@@ -6,13 +6,20 @@ def main():
   if st.button('Home'):
     st.switch_page("dataviz.py")
   # Carregar os dados
-  lista_presentes = pd.read_csv('docs_silver/lista_presentes.csv',encoding='utf-8')
+  lista_presentes = pd.read_csv('docs_silver/lista_presentes.csv',encoding='utf-8').iloc[:,1:]
 
 
   # Título
   st.title('Lsita de Presentes')
-  st.metric('Tamanho da tabela', lista_presentes.shape[0])
-  st.metric('Quantidade de colunas', lista_presentes.shape[1])
+
+  #filtros
+  col1, col2 = st.columns(2)
+  filtro_texto = col2.text_input('Filtre por um termo')
+  if filtro_texto:
+     lista_presentes = lista_presentes[
+        lista_presentes.apply(lambda row: row.astype(str).str.contains(filtro_texto, case=False).any(), axis=1)
+    ]
+  col1.metric('Qtde de resultados', lista_presentes.shape[0])
 
   st.dataframe(lista_presentes,
                hide_index=True,
