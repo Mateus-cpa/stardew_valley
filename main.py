a2ddf7f187b1abf46d823e690efe053d498dc64e
Mateus Pinheiro <mateus.mcpa@gmail.com>
2026-06-17 07:35:04 -0300

    :sparkles: feat: reorganiza as páginas

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-def pipeline():
-    #importações
-    import pandas as pd
-    from io import StringIO
-    from bs4 import BeautifulSoup as bs4
-    import numpy as np
-    #bibliotecas locais
+def init_streamlit():
+    # importação
+    import streamlit as st
+
+    # importar bibliotecas locais
     from src.extract_bs4 import extrair_sopa as extrair
-    from src.process_html_to_csv import df_cultivo
+    from src.process_html_to_csv import processamento_geral
 
-    extrair()
-    df_cultivo(html='docs\sopa_Cultivo.html')
+    st.title("Stardew Valley")
+    st.write("Bem-vindo ao Stardew Valley")
+    if st.button("iniciar scraping"):
+        extrair()  # importa html
+        # botão de limpar tela
+        if st.button("limpar tela"):
+            st.clear()
+    if st.button("processar html"):
+        processamento_geral()  # html => csv
 
 
-if __name__ == '__main__':
-    pipeline()
-    
+if __name__ == "__main__":
+    init_streamlit()
